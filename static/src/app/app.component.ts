ccfb1f01af9d755681c9e2a48d8f9ca0513b10e1
maayanfael <maayanfael@gmail.com>
2017-08-08 18:16:36 +0300

    angular 2 - does not work

---
+++ static/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {NavbarComponent} from './navbar/navbar.component';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+}
